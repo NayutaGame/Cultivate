b611b9e87c0cf9c2a385730133af549945ba0f31
Chengqi Li <335997095@qq.com>
2024-08-05 14:54:06 +0800

    formation now shows progress

--- Cultivate/Assets/__Project/Scripts/View/Formations/FormationIconView.cs
+++ Cultivate/Assets/__Project/Scripts/View/Formations/FormationIconView.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FormationIconView : SimpleView
 {
     [SerializeField] private TMP_Text NameText;
+    [SerializeField] private TMP_Text ProgressText;
 
     public override void Refresh()
     {
@@ -21,5 +22,32 @@ public class FormationIconView : SimpleView
             NameText.text = $"<style={jingJie.ToString()}>{formation.GetName()}</style>";
         else
             NameText.text = formation.GetName();
+
+        if (ProgressText != null)
+        {
+            if (formation is RunFormation f)
+            {
+                int progress = f.GetProgress();
+                
+                JingJie nextJingJie;
+                JingJie? activatedJingJie = f.GetActivatedJingJie();
+                JingJie highestJingJie = f.GetEntry().GetFormationGroupEntry().SubFormationEntries[0].GetJingJie();
+                if (!activatedJingJie.HasValue)
+                {
+                    nextJingJie = f.GetLowestJingJie();
+                }
+                else if (activatedJingJie != highestJingJie)
+                {
+                    nextJingJie = f.GetIncrementedJingJie(activatedJingJie.Value);
+                }
+                else
+                {
+                    nextJingJie = highestJingJie;
+                }
+                
+                int requirement = formation.GetRequirementFromJingJie(nextJingJie);
+                ProgressText.text = $"{progress}/{requirement}";
+            }
+        }
     }
 }
